1073e1f817a933ad73153c8766cd418e490062c5
EricXCY <eric759920033xcyss@gmail.com>
2025-05-18 22:15:42 -0700

    add tags for shield and sword

--- src/main/java/com/erix/creatorsword/ModDataGenerator.java
+++ src/main/java/com/erix/creatorsword/ModDataGenerator.java
@@ -1,9 +1,12 @@
 package com.erix.creatorsword;
 
 import com.erix.creatorsword.datagen.recipes.ModRecipe;
+import com.erix.creatorsword.datagen.tags.ModTag;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.TagsProvider;
+import net.minecraft.world.level.block.Block;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
@@ -18,7 +21,9 @@ public class ModDataGenerator {
         DataGenerator generator = event.getGenerator();
         PackOutput output = generator.getPackOutput();
         CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+        CompletableFuture<TagsProvider.TagLookup<Block>> blockTags = CompletableFuture.completedFuture(null);
         ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
         generator.addProvider(event.includeServer(),new ModRecipe(output,lookupProvider));
+        generator.addProvider(event.includeServer(),new ModTag(output, lookupProvider, blockTags, existingFileHelper));
     }
 }
---
+++ src/main/java/com/erix/creatorsword/datagen/tags/ModTag.java
@@ -0,0 +1,39 @@
+package com.erix.creatorsword.datagen.tags;
+
+import com.erix.creatorsword.CreatorSword;
+import com.erix.creatorsword.item.cogwheel_shield.CogwheelshieldItems;
+import com.erix.creatorsword.item.creator_sword.CreatorSwordItems;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.level.block.Block;
+import net.neoforged.neoforge.common.Tags;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+
+import javax.annotation.Nullable;
+import java.util.concurrent.CompletableFuture;
+
+public class ModTag extends ItemTagsProvider {
+    public ModTag(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider,
+                  CompletableFuture<TagLookup<Block>> blockTags, @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, blockTags, CreatorSword.MODID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider provider) {
+        tag(ItemTags.SWORDS)
+                .add(CreatorSwordItems.CREATOR_SWORD.get());
+        tag(Tags.Items.MELEE_WEAPON_TOOLS)
+                .add(CreatorSwordItems.CREATOR_SWORD.get());
+        tag(Tags.Items.TOOLS_WRENCH)
+                .add(CreatorSwordItems.CREATOR_SWORD.get());
+
+        tag(Tags.Items.TOOLS_SHIELD)
+                .add(CogwheelshieldItems.COGWHEEL_SHIELD.get());
+        tag(ItemTags.VANISHING_ENCHANTABLE)
+                .add(CogwheelshieldItems.COGWHEEL_SHIELD.get());
+        tag(ItemTags.DURABILITY_ENCHANTABLE)
+                .add(CogwheelshieldItems.COGWHEEL_SHIELD.get());
+    }
+}
