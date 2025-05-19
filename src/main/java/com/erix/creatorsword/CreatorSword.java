adf5f761fed0b8463fa6cf4452fc9afbcaa0a1f2
EricXCY <eric759920033xcyss@gmail.com>
2025-05-18 22:16:46 -0700

    edit commit

--- src/main/java/com/erix/creatorsword/CreatorSword.java
+++ src/main/java/com/erix/creatorsword/CreatorSword.java
@@ -11,7 +11,6 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 
-// The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(CreatorSword.MODID)
 public class CreatorSword
 {
